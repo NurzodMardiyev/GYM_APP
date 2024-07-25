1685fd9817374f9528b40ee8a591ec6e0dc37303
NurzodMardiyev <nurzodbekmardiyev1306@gmail.com>
2024-07-25 16:48:32 +0500

    first commit

---
+++ src/components/SuperLogin.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function SuperLogin() {
+  return (
+    <div>
+      <form action="">
+        <input type="text" placeholder="Your name" />
+        <input type="password" placeholder="Your password" />
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SuperLogin from "../components/SuperLogin";
+import "../App.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <SuperLogin />
+    </div>
+  );
+}
